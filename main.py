77061affe7122472608bf6ac4c2f0c5118bb35f8
Abubakir Bohodirov <abubakrbohodirov185@gmail.com>
2024-09-14 15:08:40 +0500

    done

---
+++ main.py
@@ -0,0 +1,55 @@
+i = 0
+
+while i < 10:
+    i = i + 1
+    print(i)
+
+
+
+
+
+
+
+son = int(input("Son kiriting: ")) + 1
+
+while  son > 1:
+    son = son - 1
+
+    print(son)
+
+
+
+
+
+
+son2 = int(input("son kiriting: "))
+
+summa = 0
+
+i = 1
+
+while i <= son2:
+    summa = summa + i
+    i = i + 1
+    
+print(f"1dan {son2}gacha sonlar yig'indisi {summa}ga teng.")
+
+
+
+
+
+
+
+summa = 0
+
+son3 = int(input("Son kiriting: "))
+if son3 != 0:
+   summa = summa + son3
+
+
+
+while son3 != 0:
+   son3 = int(input("Son kiriting: "))
+   summa = summa + son3
+
+print(summa)
